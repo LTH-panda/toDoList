24c95205bb2a6a7f7c73e442069d31a4fcbbc8e3
이태현 <johtta888@naver.com>
2021-01-23 08:03:43 +0000

    2021-01-23-#02

--- doItList/my_to_do_web/views.py
+++ doItList/my_to_do_web/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("index")
+    todos = ToDo.objects.all()
+    content = {'todos' : todos}
+    return render(request,'my_to_do_web/index.html', content)
+
+def createTodo(request):
+    user_input_content = request.POST['todoContent']
+    new_todo = ToDo(content = user_input_content)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("createTodo =>>" + user_input_content )
+    
+def deleteTodo(request):
+    delete_todo_id = request.GET['todoNum']
+    delete_todo = ToDo.objects.get(id = delete_todo_id)
+    delete_todo.delete()
+    return render(request, 'my_to_do_web/index.html')        
